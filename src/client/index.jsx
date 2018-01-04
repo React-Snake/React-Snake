a06971d9659ce0b2f322e8e90b342a315f66cb03
Jourdan Clark <jourdan_clark@hotmail.com>
2018-01-04 09:59:18 -0700

    Let's make a <Snake/>!

---
+++ src/client/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './styles/index.css';
+
+render(<div><p>Hello World</p></div>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+var path = require('path');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const productionPluginDefine = isProduction ? [
+  new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),
+  new webpack.DefinePlugin({
+    'process.env.BROWSER': false,
+  }),
+] : [];
+const clientLoaders = isProduction ? productionPluginDefine.concat([
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }, sourceMap: false }),
+  new webpack.DefinePlugin({
+    'process.env.BROWSER': true,
+  }),
+]) : [];
+
+module.exports = [
+  {
+    entry: ['babel-polyfill', './src/client/index.jsx'],
+    output: {
+      path: path.resolve(__dirname,'/src/public'),
+      publicPath: path.resolve(__dirname,'/src/public'),
+      filename: 'js/bundle.js'
+    },
+    plugins: clientLoaders.concat([
+      new ExtractTextPlugin('css/styles.css', {
+        allChunks: true
+      })
+    ]),
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract('css!sass')
+        },
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('css-loader')
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
+  }
+];
